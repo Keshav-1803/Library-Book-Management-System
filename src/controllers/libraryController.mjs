1d98215f9763bfc2600a1e18e1d70ee1074d372e
Keshav <keshavagarwalfabs@gmail.com>
2024-04-09 15:03:31 +0530

    Added Search Update and Delete feature

--- src/controllers/libraryController.mjs
+++ src/controllers/libraryController.mjs
@@ -2,19 +2,54 @@ import libraryModel from "../models/libraryModel.mjs";
 
 const readBooks = async (req,res) => {
    const allBooks = await libraryModel.find();
-   res.send({
+   return res.status(200).send({
       status : "Status 200 Array of books matching the search criteria",
       AllBooks : allBooks
    });
 }
 
 const addBooks = async (req,res) => {
-   const book = req.body;
-   const addedbooks = await libraryModel.create(book);
-   res.send({
-      status : "Success 201 The Book's details added successfully",
-      AddedBooks : addedbooks
-   });
+   try {
+      const book = req.body;
+      const addedbooks = await libraryModel.create(book);
+      return res.status(201).send({
+         status : "Success 201 The Book's details added successfully",
+         AddedBooks : addedbooks
+      });
+   } catch (error) {
+      return res.status().send({Status : false, Error : error.message})
+   }
 };
 
-export {addBooks, readBooks};
+const deleteBook = async (req, res) => {
+   try {
+      const bookID = req.params.id;
+      const deletedBook = await libraryModel.findByIdAndDelete(bookID);
+      return res.status(200).send({Status : true, Deleted : deletedBook});
+   } catch (error){
+      return res.status(404).send({Status : false, Error : error.message});
+   }
+}
+
+const updateBook = async (req, res) => {
+   try {
+      const bookID = req.params.id;
+      const update = req.body;
+      const updatedBook = await libraryModel.findByIdAndUpdate(bookID, update);
+      return res.status(202).send({Status : true, Updated : updatedBook});
+   } catch (error) {
+      return res.status(404).send({Status : false, Error : error.message});
+   }
+}
+
+const searchBook = async (req,res) => {
+   try {
+      const {title, author, genre, isbn, availability} = req.query;
+      const searchedBooks = await libraryModel.find({$or : [{title : title}, {author : author}, {genre : genre}, {ISBN : isbn}, {availability : availability}]});
+      return res.status(200).send({Status : true, Searched : searchedBooks});
+   } catch (error) {
+      return res.status(400).send({Status : false, Error: error.message});
+   }
+}
+
+export {addBooks, readBooks, deleteBook, updateBook, searchBook};
--- src/routes/route.mjs
+++ src/routes/route.mjs
@@ -1,8 +1,11 @@
 import express from 'express';
-import { addBooks, readBooks } from '../controllers/libraryController.mjs';
+import { addBooks, deleteBook, readBooks, searchBook, updateBook } from '../controllers/libraryController.mjs';
 
 const router = express.Router();
 router.get('/lbms/books', readBooks);
 router.post('/lbms/books/add', addBooks);
+router.delete('/lbms/books/delete/:id', deleteBook);
+router.put('/lbms/books/update/:id', updateBook);
+router.get('/lbms/books/search', searchBook);
 
 export default router;
