76d66eab467a06df8c8c023a0a6e23ff169553cf
Keshav <keshavagarwalfabs@gmail.com>
2024-04-09 15:26:17 +0530

    Status Handelling Updated

--- src/controllers/libraryController.mjs
+++ src/controllers/libraryController.mjs
@@ -17,7 +17,7 @@ const addBooks = async (req,res) => {
          AddedBooks : addedbooks
       });
    } catch (error) {
-      return res.status().send({Status : false, Error : error.message})
+      return res.status().send({Status : 400, Error : "Invalid request data"})
    }
 };
 
@@ -25,9 +25,9 @@ const deleteBook = async (req, res) => {
    try {
       const bookID = req.params.id;
       const deletedBook = await libraryModel.findByIdAndDelete(bookID);
-      return res.status(200).send({Status : true, Deleted : deletedBook});
+      return res.status(200).send({Status : 200, Message : "Book deleted successfully", Deleted : deletedBook});
    } catch (error){
-      return res.status(404).send({Status : false, Error : error.message});
+      return res.status(404).send({Status : 404, Error : "Book not found"});
    }
 }
 
@@ -36,9 +36,9 @@ const updateBook = async (req, res) => {
       const bookID = req.params.id;
       const update = req.body;
       const updatedBook = await libraryModel.findByIdAndUpdate(bookID, update);
-      return res.status(202).send({Status : true, Updated : updatedBook});
+      return res.status(202).send({Status : 202, Message : "Book updated successfully", Updated : updatedBook});
    } catch (error) {
-      return res.status(404).send({Status : false, Error : error.message});
+      return res.status(404).send({Status : 404, Error : "Book not found"});
    }
 }
 
@@ -46,9 +46,9 @@ const searchBook = async (req,res) => {
    try {
       const {title, author, genre, isbn, availability} = req.query;
       const searchedBooks = await libraryModel.find({$or : [{title : title}, {author : author}, {genre : genre}, {ISBN : isbn}, {availability : availability}]});
-      return res.status(200).send({Status : true, Searched : searchedBooks});
+      return res.status(200).send({Status : 200, Message : "Array of books matching the search criteria", Searched : searchedBooks});
    } catch (error) {
-      return res.status(400).send({Status : false, Error: error.message});
+      return res.status(400).send({Status : 400, Error: "Invalid search parameters"});
    }
 }
 
